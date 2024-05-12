72779a384afacff2fa5f39b0662842ab3c1802d2
Juniper Watson <lyravox1@gmail.com>
2024-05-12 14:03:44 -0500

    Edit fun.py

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -1,6 +1,8 @@
 import nextcord
 from nextcord.ext import commands
+from nextcord import Interaction, Member, SlashOption
 import random
+import json
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -8,7 +10,7 @@ class Fun(commands.Cog):
         self.color = 0xa8b5e0
 
     @nextcord.slash_command(description="Flip a coin!")
-    async def coinflip(self, interaction):
+    async def coinflip(self, interaction: Interaction):
         avatar = str(self.bot.user.avatar.url)
 
         result = random.choice(['Heads', 'Tails'])
@@ -22,6 +24,21 @@ class Fun(commands.Cog):
         embed.set_thumbnail(url=avatar)
 
         await interaction.response.send_message(embed=embed)
+
+    @nextcord.slash_command(description="Replies with a random topic.")
+    async def topic(self, interaction: Interaction):
+        with open("src/data/topics.json") as f:
+            topics = json.load(f)
+            topic = random.choice(topics)
+            
+            embed = nextcord.Embed(
+                title="Topic",
+                color=self.color,
+                description=f"""
+                **Conversation Topic**
+                > * **Picked:** {topic} 
+                """)
+            await interaction.response.send_message(embed=embed)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
