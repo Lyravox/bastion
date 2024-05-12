ae87967bacd25882eb178cad58d31d939d7bc748
Juniper Watson <lyravox1@gmail.com>
2024-05-11 23:23:12 -0500

    Edit utilities.py

--- src/cogs/utilities.py
+++ src/cogs/utilities.py
@@ -9,14 +9,47 @@ class Utilities(commands.Cog):
         self.bot = bot
         self.color = 0xa8b5e0
         
+    # About command
+    @nextcord.slash_command(description="About Bastion!")
+    async def about(self, interaction: Interaction):
+        avatar = str(self.bot.user.avatar.url)
+        members = sum(guild.member_count for guild in self.bot.guilds)
+        bot_user = await self.bot.fetch_user(self.bot.user.id)
+        banner = bot_user.banner.url
+        
+        
+        embed = nextcord.Embed(
+            title="About",
+            color=self.color,
+            description=f"""**Information**
+            > * **Name:** Bastion
+            > * **Owner:** <@1136085806147182713>
+            > * **ID:** {self.bot.user.id}
+            > * **Github/Documentation:** https://github.com/Lyravox/bastion
+            
+            **Statistics**
+            > * **Server Count:** {len(self.bot.guilds)}
+            > * **Member Count:** {members}
+            """
+            )
+        embed.set_thumbnail(url=avatar)
+        embed.set_image(url=banner)
+        
+        await interaction.response.send_message(embed=embed)
+        
     # Ping command
     @nextcord.slash_command(description="Replies with bot latency.")
     async def ping(self, interaction: Interaction):
         latency = int(self.bot.latency * 1000)
         avatar = self.bot.user.avatar.url
         
-        embed = nextcord.Embed(title="Ping", color=self.color, description=f"""**Bot Latency**
-                               > * **Latency:** {latency}""")
+        embed = nextcord.Embed(
+            title="Ping",
+            color=self.color,
+            description=f"""
+            **Bot Latency**
+            > * **Latency:** {latency}
+                               """)
         embed.set_thumbnail(url=avatar)
         
         await interaction.response.send_message(embed=embed)
